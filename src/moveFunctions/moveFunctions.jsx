ab99e18c85555d73fdf595ef5ecbc26aa9e33c49
SosukeYoshida <136792295+SosukeYoshida@users.noreply.github.com>
2024-02-22 02:00:07 +0900

    動作修正３

--- src/moveFunctions/moveFunctions.jsx
+++ src/moveFunctions/moveFunctions.jsx
@@ -10,29 +10,34 @@ export const rightMove = (fieldNum, prevField, marge, setIsMove, initMarge, setM
                     l++;
                     setIsMove(true);
                 }
-                let k = j - 1;
-                while (k >= 0) {
-                    // 合体
-                    if (prevField[i][k] === prevField[i][j]) {
-                        //同じ値のセルの間に別の値セルがあった場合の処理
-                        if (prevField[i][k + 1] !== prevField[i][k] && prevField[i][k + 1] !== 0) {
-                            break;
-                        }
-                        if (marge[i][j] !== true) {
-                            if (marge[i][j - 1] !== true) {
-                                prevField[i][j] *= 2;
-                                prevField[i][k] = 0;
-                                marge[i][j] = true
-                                j = k;
-                                setIsMove(true);
-                            }
-                        }
-                    }
-                    k--;
-                }
             }
         }
     }
+    for (let i = 0; i < fieldNum; i++) {
+        for (let j = fieldNum - 1; j >= 0; j--) {
+            let k = j - 1;
+            while (k >= 0) {
+                // 合体
+                if (prevField[i][k] === prevField[i][j]) {
+                    console.log(prevField[i][k + 1]);
+                    //同じ値のセルの間に別の値セルがあった場合の処理
+                    if (prevField[i][k + 1] !== prevField[i][k] && prevField[i][k + 1] !== 0) {
+                        break;
+                    }
+                    if (marge[i][j] !== true) {
+                        if (marge[i][j - 1] !== true) {
+                            prevField[i][j] *= 2;
+                            prevField[i][k] = 0;
+                            marge[i][j] = true
+                            setIsMove(true);
+                        }
+                    }
+                }
+                k--;
+            }
+        }
+    }
+
     setMarge(initMarge.map(row => [...row]));  // 新しい配列を生成してセット
     return [...prevField];
 };
@@ -49,26 +54,30 @@ export const leftMove = (fieldNum, prevField, marge, setIsMove, initMarge, setMa
                     l--;
                     setIsMove(true);
                 }
-                let k = j + 1;
-                while (k < 4) {
-                    // 合体
-                    if (prevField[i][k] === prevField[i][j]) {
-                        //同じ値のセルの間に別の値セルがあった場合の処理
-                        if (prevField[i][k - 1] !== prevField[i][k] && prevField[i][k - 1] !== 0) {
-                            break;
-                        }
-                        if (marge[i][j] !== true) {
-                            if (marge[i][j + 1] !== true) {
-                                prevField[i][j] *= 2;
-                                prevField[i][k] = 0;
-                                marge[i][j] = true;
-                                j = k;
-                                setIsMove(true);
-                            }
+            }
+        }
+    }
+    for (let i = 0; i < fieldNum; i++) {
+        for (let j = 0; j < fieldNum; j++) {
+            let k = j + 1;
+            while (k < 4) {
+                // 合体
+                if (prevField[i][k] === prevField[i][j]) {
+                    //同じ値のセルの間に別の値セルがあった場合の処理
+                    if (prevField[i][k - 1] !== prevField[i][k] && prevField[i][k - 1] !== 0) {
+                        break;
+                    }
+                    if (marge[i][j] !== true) {
+                        if (marge[i][j + 1] !== true) {
+                            prevField[i][j] *= 2;
+                            prevField[i][k] = 0;
+                            marge[i][j] = true;
+                            j = k;
+                            setIsMove(true);
                         }
                     }
-                    k++;
                 }
+                k++;
             }
         }
     }
@@ -88,26 +97,30 @@ export const upMove = (fieldNum, prevField, marge, setIsMove, initMarge, setMarg
                     l--;
                     setIsMove(true);
                 }
-                let k = i + 1;
-                while (k < 4) {
-                    // 合体
-                    if (prevField[k][j] === prevField[i][j]) {
-                        //同じ値のセルの間に別の値セルがあった場合の処理
-                        if (prevField[k - 1][j] !== prevField[k][j] && prevField[k - 1][j] !== 0) {
-                            break;
-                        }
-                        if (marge[i] && marge[i][j] !== true) {
-                            if (marge[i + 1] && marge[i + 1][j] !== true) {
-                                prevField[i][j] *= 2;
-                                prevField[k][j] = 0;
-                                marge[i][j] = true;
-                                i = k;
-                                setIsMove(true);
-                            }
+            }
+        }
+    }
+    for (let j = 0; j < fieldNum; j++) {
+        for (let i = 0; i < fieldNum; i++) {
+            let k = i + 1;
+            while (k < 4) {
+                // 合体
+                if (prevField[k][j] === prevField[i][j]) {
+                    //同じ値のセルの間に別の値セルがあった場合の処理
+                    if (prevField[k - 1][j] !== prevField[k][j] && prevField[k - 1][j] !== 0) {
+                        break;
+                    }
+                    if (marge[i] && marge[i][j] !== true) {
+                        if (marge[i + 1] && marge[i + 1][j] !== true) {
+                            prevField[i][j] *= 2;
+                            prevField[k][j] = 0;
+                            marge[i][j] = true;
+                            i = k;
+                            setIsMove(true);
                         }
                     }
-                    k++;
                 }
+                k++;
             }
         }
     }
@@ -127,26 +140,30 @@ export const downMove = (fieldNum, prevField, marge, setIsMove, initMarge, setMa
                     l++;
                     setIsMove(true);
                 }
-                let k = i - 1;
-                while (k >= 0) {
-                    // 合体
-                    if (prevField[k][j] === prevField[i][j]) {
-                        //同じ値のセルの間に別の値セルがあった場合の処理
-                        if (prevField[k + 1][j] !== prevField[k][j] && prevField[k + 1][j] !== 0) {
-                            break;
-                        }
-                        if (marge[i] && marge[i][j] !== true) {
-                            if (marge[i - 1] && marge[i - 1][j] !== true) {
-                                prevField[i][j] *= 2;
-                                prevField[k][j] = 0;
-                                marge[i][j] = true;
-                                i = k;
-                                setIsMove(true);
-                            }
+            }
+        }
+    }
+    for (let j = 0; j < fieldNum; j++) {
+        for (let i = fieldNum - 1; i >= 0; i--) {
+            let k = i - 1;
+            while (k >= 0) {
+                // 合体
+                if (prevField[k][j] === prevField[i][j]) {
+                    //同じ値のセルの間に別の値セルがあった場合の処理
+                    if (prevField[k + 1][j] !== prevField[k][j] && prevField[k + 1][j] !== 0) {
+                        break;
+                    }
+                    if (marge[i] && marge[i][j] !== true) {
+                        if (marge[i - 1] && marge[i - 1][j] !== true) {
+                            prevField[i][j] *= 2;
+                            prevField[k][j] = 0;
+                            marge[i][j] = true;
+                            i = k;
+                            setIsMove(true);
                         }
                     }
-                    k--;
                 }
+                k--;
             }
         }
     }
